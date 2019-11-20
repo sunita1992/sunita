31b8b3280b1ebe61090008681fc7d99c15fb4fd7
sunitapatil <sunitajpatil92@gmail.com>
2019-11-20 14:09:01 +0530

    Hii

---
+++ LoopExample/src/for_loop/Print_1_100.java
@@ -0,0 +1,13 @@
+package for_loop;
+
+public class Print_1_100 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("The number 1 to 100 : " );
+		for(int i=1; i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
